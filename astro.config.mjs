89e4550b6b9fb2ff5c28ad55a8a88a4403a3966b
Hussein Hegazy <husshegazydev@gmail.com>
2026-01-06 17:44:04 -0500

    Make it look like the blog ??

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,20 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(),
+		sitemap(),
+		react({
+			experimentalReactChildren: true,
+		}),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
 });
